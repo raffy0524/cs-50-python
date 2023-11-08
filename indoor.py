337aac5cbe267368ef4c3558267468762a4c4f81
Rafael Castillo <113395471+raffy0524@users.noreply.github.com>
2023-11-07 21:07:11 -0500

    Add files via upload
    indoor program from cs50 python set problems 0

---
+++ indoor.py
@@ -0,0 +1,10 @@
+#rafael castillo
+#cs50 python
+#program problems set 0 #1 
+
+# Prompt the user for input
+user_input = input("Enter your text: ")
+
+# Convert the input to lowercase and output it
+output = user_input.lower()
+print(output)
