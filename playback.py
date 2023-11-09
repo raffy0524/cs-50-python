e5d55c8161b5f5edc6eb506c45d46af0f293ebff
Rafael Castillo <113395471+raffy0524@users.noreply.github.com>
2023-11-08 20:33:55 -0500

    Add files via upload

---
+++ playback.py
@@ -0,0 +1,13 @@
+#rafael castillo
+#cs50 python
+#programs set 0 problem #2
+
+#prompt the user for input
+user_input = input("Enter your text:")
+
+#Replace space with three  period
+output = user_input.replace(" ",  "...")
+
+
+#print the modified text
+print(output)
